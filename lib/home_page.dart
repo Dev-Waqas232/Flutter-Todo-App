7d9c6404100e4e4a3b41cf17c308c35beaa17655
Waqas Munir <devwaqas232@gmail.com>
2024-07-24 21:54:12 +0500

    Added Functionality for changing checkbox

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,23 +11,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List todoList = [
+    [
+      "Make Tutorials",
+      false,
+    ],
+    ["Do Exercise", false]
+  ];
+
+  void checkboxChanged(bool? value, int index) {
+    setState(() {
+      todoList[index][1] = !todoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow[200],
-      appBar: AppBar(
-        title: const Center(child: Text("Todo App")),
-        backgroundColor: Colors.yellow,
-      ),
-      body: ListView(
-        children: [
-          TodoTile(
-            taskName: "Finish Project",
-            taskCompleted: false,
-            onChanged: (p0) {},
-          )
-        ],
-      ),
-    );
+        backgroundColor: Colors.yellow[200],
+        appBar: AppBar(
+          title: const Center(child: Text("Todo App")),
+          backgroundColor: Colors.yellow,
+        ),
+        body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            return TodoTile(
+              taskName: todoList[index][0],
+              taskCompleted: todoList[index][1],
+              onChanged: (value) => checkboxChanged(value, index),
+            );
+          },
+        ));
   }
 }
